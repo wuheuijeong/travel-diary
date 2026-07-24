cbac060cafa7752a2d4b89a598de626acdecf98e
chaei388 <day3856@gmail.com>
2026-07-24 14:58:12 +0900

    #1 App.jsx - 총 국가 수 계산 로직 작성 및 헤더 컴포넌트 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import { useState } from 'react';
 import './App.css';
+import Header from './components/Header'
 
 function App() {
+  const [travels, setTravels] = useState([])
+
+  // 총 국가 수 계산
+  const totalCountries = new Set(travels.map(t => t.country)).size;
+  
   return (
     <div className="App">
-      <h1>여행 기록</h1>
+
+      <Header
+        totalTrips={travels.length}
+        totalCountries={totalCountries}
+      />
+
     </div>
   );
 }
